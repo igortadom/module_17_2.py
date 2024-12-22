362bea8504dd62ddc6e0ccdc5a3465ba0ddbb6e6
igortadom <tadom@yandex.ru>
2024-12-22 09:13:55 +0300

    Add files via upload

---
+++ user.py
@@ -0,0 +1,17 @@
+from app.backend.db import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import CreateTable
+
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship('Task', back_populates='users')
+
+print(CreateTable(User.__table__))
